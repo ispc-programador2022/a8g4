c8d42fa1543c7efe34edc86c9c050e9f7953437a
MatiasLopez27 <matiaslopezdatascientist@gmail.com>
2022-06-21 22:35:20 +0000

    first commit

--- suma.py
+++ suma.py
@@ -2,3 +2,4 @@ value1=int(input ("Ingresar primer valor para realizar la suma"))
 value2=int(input ("Ingresar primer valor para realizar la suma"))
 resultado_suma=value1+value2
 print (resultado_suma)
+print (value1)
