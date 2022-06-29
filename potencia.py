91a277a218220051df5711374854986bf2e212ec
JMarchisone <jorgemarchisone@gmail.com>
2022-06-29 15:31:50 -0300

    Se sube funcion de potencia

---
+++ potencia.py
@@ -0,0 +1,7 @@
+"""
+Creación de una función que retorna la potencia del primero elevado al segundo parámetro.
+"""
+base = float(input("Por favor ingrese el número base: "))  # se introduce el número base
+exponente = float(input("Por favor ingrese el exponente: ")) # se introduce el exponente
+potencia = base ** exponente # operación
+print ("El resultado de la potencia de base ", base, "con exponente ", exponente, "es: ", potencia) # impresión del resultado
