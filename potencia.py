0a644f6ebd9ecca23429481d1309cd9bc7c1ba77
JMarchisone <jorgemarchisone@gmail.com>
2022-06-29 15:34:56 -0300

    Se corrige código

--- potencia.py
+++ potencia.py
@@ -4,4 +4,4 @@ Creación de una función que retorna la potencia del primero elevado al segundo
 base = float(input("Por favor ingrese el número base: "))  # se introduce el número base
 exponente = float(input("Por favor ingrese el exponente: ")) # se introduce el exponente
 potencia = base ** exponente # operación
-print ("El resultado de la potencia de base ", base, "con exponente ", exponente, "es: ", potencia) # impresión del resultado
+print ("El resultado de la potencia de base", base, "con exponente", exponente, "es:", potencia) # impresión del resultado
